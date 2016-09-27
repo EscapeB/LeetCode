7df25d6cede19cf245ad9ad22c766a6a4b069c91
EscapeB <463355954@qq.com>
2016-09-27 12:23:07 +0800

    Delete node in linked list.

---
+++ DeleteNodeInLinkedList.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Chasel on 2016/9/27.
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+    node.val=node.next.val;
+    node.next =node.next.next;
+};
