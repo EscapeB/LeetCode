d5819a08476269325430d8892f1bb25e45858739
EscapeB <463355954@qq.com>
2017-02-28 23:39:32 +0800

    change some detail

--- NumberComplement.js
+++ NumberComplement.js
@@ -7,11 +7,14 @@
  */
 var findComplement = function(num) {
     var i=1;
-    while (i<num)
+    if(num==0){return 1;}
+    else if(num==1){return 0;}
+    while (i<=num)
     {
+
         i=i<<1;
     }
     return i-1-num;
     
 };
-console.log(findComplement(5));
+console.log(findComplement(2147483647));
