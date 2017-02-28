64ae6208ad56a3fb27cabbca03f408a1c8bd56e6
EscapeB <463355954@qq.com>
2017-02-28 23:45:04 +0800

    For problem NumberComplement

--- NumberComplement.js
+++ NumberComplement.js
@@ -7,14 +7,15 @@
  */
 var findComplement = function(num) {
     var i=1;
-    if(num==0){return 1;}
+    /*if(num==0){return 1;}
     else if(num==1){return 0;}
+    else if(num==Math.max()){return 0;}*/
     while (i<=num)
     {
 
         i=i<<1;
     }
-    return i-1-num;
+    return ~num&(i-1);
     
 };
 console.log(findComplement(2147483647));
